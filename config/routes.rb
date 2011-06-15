447e6ea2b6363e02a98d666f4ea76e2dd11116d4
Daniele Amico <dadoam92@gmail.it>
2011-06-15 14:32:06 +0200

    added the word \"articoli\" for displaying the post
    localhost:3000/articoli => localhost:3000/posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Blog::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+   match 'articoli' => 'posts#index'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
